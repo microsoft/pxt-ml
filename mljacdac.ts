02c70a09a89e70cf3ef467e8b0c1d33ef8343604
Michal Moskal <michal@moskal.me>
2020-09-22 13:16:49 +0200

    Single ML service

--- mljacdac.ts
+++ mljacdac.ts
@@ -1,8 +1,18 @@
+namespace jd_class {
+    export const MODEL_RUNNER = 0x140f9a78
+}
+
 namespace jacdac {
-    export enum TFLiteCmd {
+    export enum ModelRunnerModelFormat { // uint32_t
+        TFLite = 0x334c4654,
+        ML4F = 0x30470f62,
+    }
+
+    export enum ModelRunnerCmd {
         /**
          * Argument: model_size bytes uint32_t. Open pipe for streaming in the model. The size of the model has to be declared upfront.
-         * The model is streamed over regular pipe data packets, in the `.tflite` flatbuffer format.
+         * The model is streamed over regular pipe data packets.
+         * The format supported by this instance of the service is specified in `format` register.
          * When the pipe is closed, the model is written all into flash, and the device running the service may reset.
          */
         SetModel = 0x80,
@@ -14,7 +24,7 @@ namespace jacdac {
         Predict = 0x81,
     }
 
-    export enum TFLiteReg {
+    export enum ModelRunnerReg {
         /**
          * Read-write uint16_t. When register contains `N > 0`, run the model automatically every time new `N` samples are collected.
          * Model may be run less often if it takes longer to run than `N * sampling_interval`.
@@ -38,11 +48,28 @@ namespace jacdac {
         /** Read-only bytes uint32_t. Number of RAM bytes allocated for model execution. */
         AllocatedArenaSize = 0x183,
 
-        /** Read-only bytes uint32_t. The size of `.tflite` model in bytes. */
+        /** Read-only bytes uint32_t. The size of the model in bytes. */
         ModelSize = 0x184,
 
         /** Read-only string (bytes). Textual description of last error when running or loading model (if any). */
         LastError = 0x185,
+
+        /**
+         * Constant ModelFormat (uint32_t). The type of ML models supported by this service.
+         * `ModelRunner` is flatbuffer `.tflite` file.
+         * `ML4F` is compiled machine code model for Cortex-M4F.
+         * The format is typically present as first or second little endian word of model file.
+         */
+        Format = 0x186,
+
+        /** Constant uint32_t. A version number for the format. */
+        FormatVersion = 0x187,
+
+        /**
+         * Constant bool (uint8_t). If present and true this service can run models independently of other
+         * instances of this service on the device.
+         */
+        Parallel = 0x188,
     }
 
     export function packArray(arr: number[], fmt: NumberFormat) {
@@ -59,9 +86,10 @@ namespace jacdac {
         protected outputs = Buffer.create(0)
         protected lastError: string
         protected lastRunNumSamples = 0
+        protected formatVersion = 0
 
-        constructor(nam: string, classNum: number, protected agg: SensorAggregatorHost) {
-            super(nam, classNum);
+        constructor(nam: string, protected format: number, protected agg: SensorAggregatorHost) {
+            super(nam, jd_class.MODEL_RUNNER);
             agg.newDataCallback = () => {
                 if (this.autoInvokeSamples && this.lastRunNumSamples >= 0 &&
                     this.numSamples - this.lastRunNumSamples >= this.autoInvokeSamples) {
@@ -89,11 +117,11 @@ namespace jacdac {
             else return 0
         }
 
-        protected invokeModel() {}
+        protected invokeModel() { }
         protected eraseModel() {
             binstore.erase()
         }
-        protected loadModelImpl() {}
+        protected loadModelImpl() { }
 
         get inputShape(): number[] {
             return null
@@ -114,7 +142,7 @@ namespace jacdac {
             this.invokeModel()
             this.execTime = control.micros() - t0
             this.lastRunNumSamples = numSamples
-            this.sendReport(JDPacket.from(CMD_GET_REG | TFLiteReg.Outputs, this.outputs))
+            this.sendReport(JDPacket.from(CMD_GET_REG | ModelRunnerReg.Outputs, this.outputs))
         }
 
         start() {
@@ -171,24 +199,27 @@ namespace jacdac {
         }
 
         handlePacket(packet: JDPacket) {
-            this.handleRegInt(packet, TFLiteReg.AllocatedArenaSize, this.arenaBytes)
-            this.handleRegInt(packet, TFLiteReg.LastRunTime, this.execTime)
-            this.handleRegInt(packet, TFLiteReg.ModelSize, this.modelSize)
-            this.handleRegBuffer(packet, TFLiteReg.Outputs, this.outputs)
-            this.autoInvokeSamples = this.handleRegInt(packet, TFLiteReg.AutoInvokeEvery, this.autoInvokeSamples)
+            this.handleRegInt(packet, ModelRunnerReg.AllocatedArenaSize, this.arenaBytes)
+            this.handleRegInt(packet, ModelRunnerReg.LastRunTime, this.execTime)
+            this.handleRegInt(packet, ModelRunnerReg.ModelSize, this.modelSize)
+            this.handleRegInt(packet, ModelRunnerReg.Format, this.format)
+            this.handleRegInt(packet, ModelRunnerReg.FormatVersion, this.formatVersion)
+            this.handleRegBool(packet, ModelRunnerReg.Parallel, false)
+            this.handleRegBuffer(packet, ModelRunnerReg.Outputs, this.outputs)
+            this.autoInvokeSamples = this.handleRegInt(packet, ModelRunnerReg.AutoInvokeEvery, this.autoInvokeSamples)
 
             let arr: number[]
             switch (packet.service_command) {
-                case TFLiteCmd.SetModel:
+                case ModelRunnerCmd.SetModel:
                     control.runInBackground(() => this.readModel(packet))
                     break
-                case TFLiteReg.OutputShape | CMD_GET_REG:
+                case ModelRunnerReg.OutputShape | CMD_GET_REG:
                     arr = this.outputShape
-                case TFLiteReg.InputShape | CMD_GET_REG:
+                case ModelRunnerReg.InputShape | CMD_GET_REG:
                     arr = arr || this.inputShape
                     this.sendReport(JDPacket.from(packet.service_command, packArray(arr, NumberFormat.UInt16LE)))
                     break;
-                case TFLiteReg.LastError | CMD_GET_REG:
+                case ModelRunnerReg.LastError | CMD_GET_REG:
                     this.sendReport(JDPacket.from(packet.service_command, Buffer.fromUTF8(this.lastError || "")))
                     break
                 default:
